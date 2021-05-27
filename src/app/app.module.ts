197d9c330aaed331c4518a463fb390a38356c5c8
Ganesh Chougule <ganesh.chougule@netcore.co.in>
2021-05-27 15:51:13 +0530

    link component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LinkComponent } from './components/link/link.component';
+import { MatSelectComponent } from './mat-select/mat-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkComponent,
+    MatSelectComponent
   ],
   imports: [
     BrowserModule
